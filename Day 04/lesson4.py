60bed2aeb3d97b31b896aa9ab763ece1f5316381
gorillamonkebecauseyes <avqofashvili27@gmail.com>
2024-02-17 21:19:35 +0400

    Day 04

---
+++ Day 04/lesson4.py
@@ -0,0 +1,9 @@
+Name = "Luka"
+Surname = "Avqofashvili"
+Age = 12
+AgeIn10Years = 22
+
+print("my name is" ,(Name))
+print("my surname is" ,(Surname))
+print("my age is" ,(Age))
+print("my age in 10 years will be" ,(AgeIn10Years))
