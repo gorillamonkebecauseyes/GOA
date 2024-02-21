ac3c8dac70b78515898e47e285b6fc4faf6d34c3
gorillamonkebecauseyes <avqofashvili27@gmail.com>
2024-02-21 13:13:13 +0400

    Day 05

---
+++ Day 05/lesson5.py
@@ -0,0 +1,9 @@
+Luka = 12
+Gabriel = 9
+Natalia = 40
+Kakha = 38 #(კახა)
+LukaAgeIn5Years = (Luka + 5)
+GabrielAgeIn5Years = (Gabriel + 5)
+NataliaAgeIn5Years = (Natalia + 5)
+KakhaAgeIn5Years = (Kakha + 5)
+print(LukaAgeIn5Years)
