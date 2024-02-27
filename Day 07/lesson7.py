b45bc8fda9ecfedbae90a95ccefe9a83fc330731
gorillamonkebecauseyes <avqofashvili27@gmail.com>
2024-02-27 15:00:04 +0400

    Day 06

---
+++ Day 07/lesson7.py
@@ -0,0 +1,8 @@
+Age1 = int(input("EnterYourAge: "))
+Age2 = int(input("EnterYourAge: "))
+Age3 = int(input("EnterYourAge: "))
+Age4 = int(input("EnterYourAge: "))
+Age5 = int(input("EnterYourAge: "))
+
+print(Age1 * Age2 / Age3)
+print(Age1 * Age2 + Age4 + Age5)
