e7a2fbac4ba20a400b8d00dbef3af854e3edcbe5
gorillamonkebecauseyes <avqofashvili27@gmail.com>
2024-03-09 12:55:32 +0400

    day 11

---
+++ Day 11/lesson11.py
@@ -0,0 +1,8 @@
+MomAge = input("Enter your mom's age: ")
+DadAge = input("Enter your dad's age: ")
+
+if MomAge > DadAge:
+    print("Mom is older than dad")
+
+if MomAge < DadAge:
+    print("Dad is older than mom")
