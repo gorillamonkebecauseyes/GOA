c40fb338ccac3423f14d1baf68fbfed9c5f56196
gorillamonkebecauseyes <avqofashvili27@gmail.com>
2024-04-01 16:50:20 +0400

    day 17

---
+++ hackaton/hackaton1.py
@@ -0,0 +1,73 @@
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter a second number: "))
+# print(num1 + num2)
+
+
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter a second number: "))
+# print()
+
+
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter a second number: "))
+# print(num1 / num2)
+
+
+# length = int(input("Enter length of square: "))
+# width = int(input("Enter width of square: "))
+# print((length + width) * 2)
+
+
+# string1 = str(input("Enter a string: "))
+# string2 = str(input("Enter a second string: "))
+# print(string1 + ("-") + string2)
+
+
+
+
+
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter a second number: "))
+# if num1 > num2:
+#     print("First number is bigger than second number")
+# else:
+#     print("Second number is bigger than first number")
+
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# num3 = int(input("Enter third number: "))
+# if 
+
+
+# num1 = int(input("Enter a number: "))
+# if num1 < 0:
+#     print(num1 - num1 - num1)
+# else:
+#     print("The number that you entered is positive, therefore it is acceptable.")
+
+
+
+
+
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter a second number: "))
+# for i in range(num1, num2):
+#     print(i * 2)
+
+
+# num = print(int(input("Enter a number: ")))                                              #not working currently
+# if num > 0 or num < 0:
+#     print(int(input("Enter a number: ")))
+# else:
+#     print("The number you entered is exquisitely zero. good job!")
+# if num > 0 or num < 0:
+#     print(int(input("Enter a number: ")))
+# else:
+#     print("The number you entered is exquisitely zero. good job!")
+# if num > 0 or num < 0:
+#     print(int(input("Enter a number: ")))
+# else:
+#     print("The number you entered is exquisitely zero. good job!")
+
+
