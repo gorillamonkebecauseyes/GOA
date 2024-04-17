b1a7d6ff235676e32e3e5dedde788b59b1d96451
gorillamonkebecauseyes <avqofashvili27@gmail.com>
2024-04-17 19:13:22 +0400

    commit

---
+++ Day 22/classworks/Classworks.py
@@ -0,0 +1,6 @@
+bestacademy = print(input("What is the best academy? "))
+
+while bestacademy == "GOA":
+    print("GOAA")
+else:
+    bestacademy = print(input("What is the best academy? "))
---
+++ Day 22/homeworks/homeworks.py
@@ -0,0 +1,4 @@
+tickets =  (int(input("ticket amount: ")))
+while tickets > 0:
+    print("WHO WANTS TICKETS FOR THE TICKET BUYING SHOW WHICH REQUIRES TICKETS?")
+    tickets = tickets - 1
