1cd2bfa69fb54067e6764f78c4e177d4f122e045
gorillamonkebecauseyes <avqofashvili27@gmail.com>
2024-05-02 11:54:13 +0400

    commit

---
+++ Day 26/classworks/Homeworks.py
@@ -0,0 +1,9 @@
+GOA_question = str(input("What academy do you go to: "))
+if GOA_question == "GOA":
+    group = input("What group are you in: ")
+if group == "13":
+    mentor = input("Is gabriel your mentor? ")
+if mentor == "yes":
+    print("I learn there too and gabriel is also my mentor.")
+if mentor == "no":
+    print("You are lying and you are not in group 13.")
