225f5133d6fbddcb0a95758c5acd363af82c5f03
gorillamonkebecauseyes <avqofashvili27@gmail.com>
2024-05-16 15:46:03 +0400

    commit

---
+++ Day 30/homeworks/lesson30.1.py
@@ -0,0 +1,3 @@
+cars = ["mercedes", "lamborgini", "mini cooper", "porsche", "toyota", "tesla",]
+print("my favourite car is " + (cars[2]))
+print("my least favourite car is " + (cars[0]))
---
+++ Day 30/homeworks/lesson30.2.py
@@ -0,0 +1,7 @@
+cars = ["mercedes", "lamborgini", "mini cooper", "porsche", "toyota", "tesla",]
+print(cars[0])
+print(cars[1])
+print(cars[2])
+print(cars[3])
+print(cars[4])
+print(cars[5])
