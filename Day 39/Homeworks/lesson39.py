de59c21d8460d6f74822ef70b6010c977d6b3d78
gorillamonkebecauseyes <avqofashvili27@gmail.com>
2024-06-19 01:52:08 +0400

    commit

---
+++ Day 39/Homeworks/lesson39.py
@@ -0,0 +1,60 @@
+num1 = int(input("enter number: "))
+num2 = int(input("enter number: "))
+num3 = int(input("enter number: "))
+num4 = int(input("enter number: "))
+num5 = int(input("enter number: "))
+num6 = int(input("enter number: "))
+num7 = int(input("enter number: "))
+num8 = int(input("enter number: "))
+num9 = int(input("enter number: "))
+num10 = int(input("enter number: "))
+
+if num1 < 100:
+    print("num1 < 100")
+else:
+    print("num1 > 100")
+
+if num2 < 100:
+    print("num2 < 100")
+else:
+    print("num2 > 100")
+
+if num3 < 100:
+    print("num3 < 100")
+else:
+    print("num3 > 100")
+
+if num4 < 100:
+    print("num4 < 100")
+else:
+    print("num4 > 100")
+
+if num5 < 100:
+    print("num5 < 100")
+else:
+    print("num5 > 100")
+
+if num6 < 100:
+    print("num6 < 100")
+else:
+    print("num6 > 100")
+
+if num7 < 100:
+    print("num7 < 100")
+else:
+    print("num7 > 100")
+
+if num8 < 100:
+    print("num8 < 100")
+else:
+    print("num8 > 100")
+
+if num9 < 100:
+    print("num9 < 100")
+else:
+    print("num9 > 100")
+
+if num10 < 100:
+    print("num10 < 100")
+else:
+    print("num10 > 100")
